cc86310edf4f178cbb7c3f904b221514596cd3df
sijan joshi <joshisijan96@gmail.com>
2021-10-25 17:00:08 +0545

    Update Navbar.js

--- src/Containers/MainLayout/Navbar/Navbar.js
+++ src/Containers/MainLayout/Navbar/Navbar.js
@@ -93,7 +93,7 @@ const Navbar = () => {
                     <MenuIcon className="w-5 h-5" /> <span>All Categories</span>
                 </MenuLink>
                 {
-                    categoryListState.list.results.slice(0, 4).map(category => {
+                    categoryListState.list.results.map(category => {
                         return (
                             <Link key={category.slug} to={`category/${category.slug}`}>
                                 <MenuLink>
