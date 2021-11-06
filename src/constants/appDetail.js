bcb16e36a2ed78b47759bdea661f724a920fb7d7
sijan joshi <joshisijan96@gmail.com>
2021-11-06 10:00:48 +0545

    Update appDetail.js

--- src/constants/appDetail.js
+++ src/constants/appDetail.js
@@ -1,8 +1,8 @@
 const appDetail = {
-    appName: "Baby Shpp",
+    appName: "Baby Shop",
     appMoto: "Everything for baby",
-    appShortDescription: "Dolore ad elit proident nulla sunt ut cillum aliquip minim amet aliqua fugiat.",
-    appDescription: "Do deserunt exercitation amet irure nostrud cupidatat. Ea reprehenderit aliquip anim aliqua qui aliqua pariatur duis cillum sit. Quis cillum cupidatat et do cupidatat esse ullamco non minim. Aliqua deserunt commodo id consequat esse excepteur ad nisi. Est in officia proident aliquip irure ut eiusmod nulla magna consequat nostrud sit laborum. Tempor aute excepteur deserunt officia. Ex minim non consequat veniam esse cupidatat dolor pariatur aute eiusmod id excepteur.",
+    appShortDescription: "A complete solution for everything you need for a baby.",
+    appDescription: "Founded in 2020 baby shop is the best site that you can get for children clothing and accessories. With the mixture of affordable and high quality goods you will never return empty handed.",
 }
 
 export default appDetail
